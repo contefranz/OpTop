741f8467ad48dbc79f13876039ec17f6d4c97938
Mattia Tamellini <mattia.tamellini@gmail.com>
2021-03-29 21:14:01 +0200

    Fixes #2, at least for now; a complete re-engineering/overhaul of the algorithm in the core loop would be needed to gain more performance than this, and even then improvements might be marginal, wrt to the effort needed

--- src/optimal_topic_core.cpp
+++ src/optimal_topic_core.cpp
@@ -10,9 +10,7 @@ arma::mat optimal_topic_core(const Rcpp::List& lda_models,
                              int n_docs,
                              int n_features)
 {
-    // Rcpp::Rcout << "ENTERED RCPP CODE" << std::endl;
-  
-    // this is the output of this method 
+    // this is the output of this method
     arma::mat Chi_K(lda_models.size(), 3);
 
     // loop variables
@@ -22,102 +20,87 @@ arma::mat optimal_topic_core(const Rcpp::List& lda_models,
         Rcpp::checkUserInterrupt();
 
         // getting the document word weights
-        Rcpp::S4 current_lda = lda_models[i_mod];
-        arma::mat dtw = current_lda.slot("gamma");
+        const Rcpp::S4& current_lda = lda_models[i_mod];
+        const arma::mat& dtw = current_lda.slot("gamma");
+        const arma::mat& beta = current_lda.slot("beta");
+        arma::mat tww = exp(beta).t();
         int current_k = dtw.n_cols;
+        // TODO TODO TODO
+        // TODO current_k is useless, actually; there used to be a check at the
+        // end when computing chi_out returns, that returned true for all rows;
+        // I'm keeping current_k in Chi_J just because the calling function
+        // expects a column "topic" in the matrix returned, and I don't know if
+        // that's really needed and why
+        // TODO TODO TODO
 
         Rcpp::Rcout << "---" << std::endl;
         Rcpp::Rcout << "# # # Processing LDA with k = " << current_k << std::endl;
 
-        // getting the term word weights -> beta
-        arma::mat tww = exp(Rcpp::as<arma::mat>(current_lda.slot("beta"))).t();
-
-        // adding row position to both objects
-        // TODO could this be optimized further by creating these two matrices in
-        // one go? or does armadillo take care of this seamlessly?
-        arma::vec tmp1(dtw.n_rows);
-        arma::vec tmp2(tww.n_rows);
-        std::iota(tmp1.begin(), tmp1.end(), 1);
-        std::iota(tmp2.begin(), tmp2.end(), 1);
-        dtw.insert_cols(dtw.n_cols, tmp1);
-        tww.insert_cols(tww.n_cols, tmp2);
-
         // looping over each document (k) in each model (j);
         // this is the loop that needs to be parallelized
-        // Rcpp::Rcout << "--> Processing documents" << std::endl;
+        Rcpp::Rcout << "--> Processing documents" << std::endl;
         for (std::size_t j_doc = 0; j_doc < n_docs; ++j_doc)
         {
             Rcpp::checkUserInterrupt();
-            // TODO logic unclear; could all of this loop be written with fewer calls?
 
-            // subsetting word proportions and dtw based on id_doc;
-            // Rcpp::Rcout << "weighted_dfm row " << j_doc << ": " << std::endl << weighted_dfm.row(j_doc) << std::endl; // TODO REMOVE
-            // define BestPair as n_cols(weighted_dfm) x 1, then fill it with weighted_dfm jth
-            arma::mat BestPair(weighted_dfm.n_cols, 1);
-            for (std::size_t row_index = 0; row_index < BestPair.n_rows; ++row_index)
+            // subsetting dtw based on id_doc
+            // TODO TODO TODO
+            // TODO is there a way to make this sparse?? there should be; why are
+            // we using all the zeros in the computation of chi_sq_fit below??
+            // That way, we could use a reference to the row instead
+            // TODO TODO TODO
+            arma::vec weighted_dfm_j_doc(weighted_dfm.n_cols);
+            const auto& row_j = weighted_dfm.row(j_doc);
+            for (std::size_t row_index = 0; row_index < weighted_dfm_j_doc.size(); ++row_index)
             {
-                BestPair(row_index, 0) = weighted_dfm.row(j_doc)(row_index);
+                weighted_dfm_j_doc(row_index) = row_j(row_index);
             }
-            // Rcpp::Rcout << "Best Pair: " << std::endl << BestPair << std::endl; // TODO REMOVE
-
-            // casting N x K matrix
-            arma::mat dtw_j_doc(n_features, dtw.n_cols);
-            for (std::size_t row_index = 0; row_index < dtw_j_doc.n_rows; ++row_index)
-            {
-              dtw_j_doc.row(row_index) = dtw.row(j_doc);
-            }
-            // Rcpp::Rcout << "dtw_j_doc: " << std::endl << dtw_j_doc << std::endl; // TODO REMOVE
-
-            // this avoids the use of j index which does not match with matlab code
-            // in matlab j loops over k_start -> k_end
-            // here starts from 1 up to the latest model
-            arma::mat sub_dtw_j_doc = dtw_j_doc.cols(0, dtw_j_doc.n_cols - 2);
-            // Rcpp::Rcout << "sub_dtw_j_doc: " << std::endl << sub_dtw_j_doc << std::endl; // TODO REMOVE
-            arma::mat sub_tww = tww.cols(0, tww.n_cols - 2);
-            // Rcpp::Rcout << "sub_tww: " << std::endl << sub_tww << std::endl; // TODO REMOVE
 
             // element-wise multiplication
-            arma::mat tww_dtw = sub_dtw_j_doc % sub_tww;
-            // Rcpp::Rcout << "tww_dtw: " << std::endl << tww_dtw << std::endl; // TODO REMOVE
+            // TODO this is the bottleneck now! But both tww and dtw are full, aren't they? There's
+            // not much we can do
+            arma::mat tww_dtw = tww.each_row() % dtw.row(j_doc);
 
-            // this returns a vector...maybe we want a matrix
+            // get cumulative sum of sorted sums of rows of tww_dtw
             arma::vec X = arma::sum(tww_dtw, 1);
-            // Rcpp::Rcout << "X: " << std::endl << X << std::endl; // TODO REMOVE
-            BestPair.insert_cols(BestPair.n_cols, X);
-            BestPair = BestPair.rows(arma::sort_index(-BestPair.col(1)));
-            // Rcpp::Rcout << "BestPair: " << std::endl << BestPair << std::endl; // TODO REMOVE
+            // TODO second bottleneck (roughly half the time as tww_dtw)
+            arma::uvec sort_indices = arma::sort_index(X, "descend");
 
-            // compute the cumulative probability over estimations
-            BestPair.insert_cols(BestPair.n_cols, arma::cumsum(BestPair.col(1)));
-            // Rcpp::Rcout << "BestPair: " << std::endl << BestPair << std::endl; // TODO REMOVE
-            int n_BP = BestPair.n_rows;
+            X = X(sort_indices);
+            arma::vec X_cumsum = arma::cumsum(X);
+            weighted_dfm_j_doc = weighted_dfm_j_doc(sort_indices);
 
-            // stop when you reach q
-            arma::mat AggBestPair = BestPair.rows(arma::find(arma::round(BestPair.col(2) * 1e4) / 1e4 <= q));
-            // Rcpp::Rcout << "AggBestPair: " << std::endl << AggBestPair << std::endl; // TODO REMOVE
-            int icut = AggBestPair.n_rows;
-            AggBestPair.insert_rows(AggBestPair.n_rows, arma::sum(BestPair.rows(icut, n_BP - 1), 0));
-            // Rcpp::Rcout << "AggBestPair: " << std::endl << AggBestPair << std::endl; // TODO REMOVE
-            double chi_sq_fit = icut * arma::sum(
-              (AggBestPair.col(0) - AggBestPair.col(1)) % (AggBestPair.col(0) - AggBestPair.col(1))
-              / AggBestPair.col(1)
+            // find elements with estimated probability lower than quantile
+            auto first_greater_than_q = std::find_if(X_cumsum.begin(),
+                                                     X_cumsum.end(),
+                                                     [&q](double val) {return round(val * 1e4) / 1e4 > q;});
+            std::size_t icut = std::distance(X_cumsum.begin(), first_greater_than_q);
+            std::size_t n_cut_elements = X.size() - icut;
+
+            // compute the sum of the rest of the elements
+            double sum_of_cut_weighted_dfm = arma::sum(weighted_dfm_j_doc.tail(n_cut_elements));
+            double sum_of_cut_X = arma::sum(X.tail(n_cut_elements));
+            double chi_sq_fit = icut * (
+                arma::sum(
+                          (
+                           (weighted_dfm_j_doc.head(icut) - X.head(icut))
+                           %
+                           (weighted_dfm_j_doc.head(icut) - X.head(icut))
+                          ) / X.head(icut)
+                         )
+                +
+                (sum_of_cut_weighted_dfm - sum_of_cut_X) * (sum_of_cut_weighted_dfm - sum_of_cut_X) / sum_of_cut_X
             );
-            // Rcpp::Rcout << "chi_sq_fit: " << std::endl << chi_sq_fit << std::endl; // TODO REMOVE
 
             regstats.row(j_doc) = arma::rowvec({double(current_k), double(j_doc), chi_sq_fit, double(icut)});
-            // Rcpp::Rcout << "regstats: " << std::endl << regstats << std::endl; // TODO REMOVE
         }
 
-        arma::mat chi_out = regstats.rows(arma::find(regstats.col(0) == double(current_k)));
-        // Rcpp::Rcout << "chi_out: " << std::endl << chi_out << std::endl; // TODO REMOVE
         Chi_K.row(i_mod) = arma::rowvec({
-          double(current_k),
-          arma::sum(chi_out.col(2)) / arma::sum(chi_out.col(3)),
-          R::pchisq(arma::sum(chi_out.col(2)) / arma::sum(chi_out.col(3)), 1, true, false)
+            double(current_k),
+            arma::sum(regstats.col(2)) / arma::sum(regstats.col(3)),
+            R::pchisq(arma::sum(regstats.col(2)) / arma::sum(regstats.col(3)), 1, true, false)
         });
-        // Rcpp::Rcout << "Chi_K: " << std::endl << Chi_K << std::endl; // TODO REMOVE
     }
 
-    // Rcpp::Rcout << "EXITING RCPP CODE" << std::endl;
     return Chi_K;
 }
